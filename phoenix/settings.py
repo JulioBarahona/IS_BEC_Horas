ba57489fdf95c13a188276d2e6b17f21c6ca3b09
Adolfo Morales <gadolfomoralesmartinez@gmail.com>
2017-11-20 15:53:59 -0600

    init

--- phoenix/settings.py
+++ phoenix/settings.py
@@ -81,9 +81,9 @@ WSGI_APPLICATION = 'phoenix.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'UVG',
+        'NAME': 'test',
         'USER': 'postgres',
-        'PASSWORD': 'chipete07',
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'TEST': {
